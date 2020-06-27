256452449cb16ccd092e091f69d15b59f54a49a7
Billy Price <billy@Billys-iMac.local>
2020-06-27 16:56:19 +1000

    COMP30014 submission

--- src/category.lean
+++ src/category.lean
@@ -4,65 +4,149 @@ The associated category of a type theory
 Author: Billy Price
 -/
 import category_theory.category
-import TL
+import definitions
 import lemmas
 
 
 
 namespace TT
 
-section 
-  variable A : type
+/-! ### tset -/
 
-  def closed : type → term → Prop := WF list.nil
+  /- Closed terms of type 𝒫 A.
+    An α : tset A is basically a set of A's, i.e. "term-set"
+  -/
+  def tset (A: type) : Type := {α : term // closed (𝒫 A) α}
 
-  def tset (A: type) := {α : term // WF [] (𝒫 A) α}
+  -- constructor
+  def tset.mk (A : type) (α : term) : closed (𝒫 A) α → tset A:= subtype.mk α
 
-  def tset.mk (α : term) : closed (𝒫 A) α → tset A:= subtype.mk α
+  -- coerce tsets to terms
+  instance tset_has_coe (A : type) : has_coe (tset A) term := coe_subtype
 
-  instance tset_has_coe : has_coe (tset A) term := coe_subtype
+  @[WF_rules]
+  def WF.tset {A} {Γ} (α : tset A) : WF Γ (𝒫 A) α
+    := WF.closed_add_context α.property
 
-  def empty_tset (A : type) : tset A := tset.mk A ⟦A | ⊥⟧ (by apply_rules WF_rules)
+  -- The empty set of any type
+  def empty_tset (A : type) : tset A := tset.mk A ⟦ A | ⊥ ⟧ (by WF_prover)
 
-  def singleton_star : tset Unit := tset.mk Unit ⟦Unit | ⊤⟧ (by apply_rules WF_rules)
+  -- The universal set of any type
+  def univ_tset (A : type) : tset A := tset.mk A ⟦ A | ⊤ ⟧ (by WF_prover)
 
-  def is_graph {A B: type} (α : tset A) (β : tset B) (F : tset (A ×× B)) : Prop :=
-    ⊨ (F ⊆[𝒫 A] (term_prod A B α β)) ⋀ (∀' A ((𝟘 ∈ α) ⟹ (∃!' B $ ⟪𝟙,𝟘⟫ ∈ F)))
+  def tset_eq {A} (α₁ α₂ : tset A) : term := term_eq 𝒫 A α₁ α₂
+  infix ` ≃ ` := tset_eq
 
-  def graph {A B} (α : tset A) (β : tset B) := {F : tset (A ×× B) // is_graph α β F}
+  def tset_subset {A} (α₁ α₂ : tset A) : term := term_subset A α₁ α₂
+  infix ` ⊆ ` := tset_subset
+
+  def tset_prod {A B} (α : tset A) (β : tset B) : tset (A 𝕏 B)
+    := tset.mk (A 𝕏 B) (term_prod A B α β) (by WF_prover;refl)
+  infix ` 𝕏 ` := tset_prod
+
+
+/-! ### graph -/
+section
   
-  instance graph_has_tset_coe (A B : type) (α : tset A) (β : tset B) : has_coe (graph α β) (tset (A ×× B)) := coe_subtype
+  variables {A B : type}
 
-  def graph.mk {A B} {α : tset A} {β : tset B} (F : tset (A ×× B)) : is_graph α β F → graph α β := subtype.mk F
+  /- F is a tset representing the graph of a function from α to β -/
+  def is_graph (α : tset A) (β : tset B) (F : tset (A 𝕏 B)) : Prop :=
+    (⊨ F ⊆ α 𝕏 β)
+      ∧
+    (⊨ ∀' A ((↑0 ∈ α) ⟹ (∃!' B $ ⟪↑1,↑0⟫ ∈ F))) -- F is functional
 
-  def id_graph {A} (α : tset A) : graph α α :=
-  begin
-    refine graph.mk _ _,
-    refine tset.mk _ (term_prod A A α α) _,
-    apply_rules WF_rules,
-    any_goals {apply rfl},
-    sorry,
-    sorry,
-    sorry,
-  end
+  /- The Type of graphs -/
+  def graph (α : tset A) (β : tset B) : Type
+    := {F : tset (A 𝕏 B) // is_graph α β F}
 
-  def composition {A B E: type} {α : tset A} {β : tset B} {η : tset E} (F : graph α β) (G : graph β η)  : graph α η :=
-  begin
-    fapply graph.mk,
-    apply tset.mk (A ×× E) ⟦ A ×× E | ∃' A (∃' E ((𝟚 ≃[A ×× E] ⟪𝟙,𝟘⟫) ⋀ (∃' B ((⟪𝟚,𝟘⟫ ∈ F) ⋀ (⟪𝟘, 𝟙⟫ ∈ G)))))⟧,
-    apply WF.comp,
-    apply WF.ex,
-    apply WF.ex,
-    apply WF.and,
-    {apply WF.eq_intro, apply WF.var, refl, apply WF.pair, apply WF.var, refl, apply WF.var, refl},
-    apply WF.ex,
-    apply WF.and,
-      {apply WF.elem, apply WF.pair, apply WF.var;refl, apply WF.var;refl, sorry},
-      {apply WF.elem, apply WF.pair, apply WF.var;refl, apply WF.var;refl, sorry},
-    sorry
-  end
+  -- graphs are tsets (are terms)
+  instance graph_has_coe {α : tset A} {β : tset B}
+    : has_coe (graph α β) (tset (A 𝕏 B)) := coe_subtype
 
-  #check id_graph 
+  def graph_eq {α : tset A} {β : tset B} (F₁ F₂ : graph α β) : term
+    := @tset_eq (A 𝕏 B) F₁ F₂
+  infix ` ≃ ` := graph_eq
+
+  def graph_subset {α : tset A} {β : tset B} (F₁ F₂ : graph α β) : term
+    := @tset_eq (A 𝕏 B) F₁ F₂
+  infix ` ⊆ ` := graph_subset
+
+  -- constructor
+  def graph.mk {α : tset A} {β : tset B} (F : tset (A 𝕏 B))
+    : is_graph α β F → graph α β := subtype.mk F
+
+  @[WF_rules]
+  def WF.graph {A B Γ} {α : tset A} {β : tset B} {F : graph α β}
+    : WF Γ (𝒫 (A 𝕏 B)) F := WF.closed_add_context F.val.property
+
+  @[WF_rules]
+  def WF.elem_graph {A B Γ} {α : tset A} {β : tset B} {ab : term} {F : graph α β}
+    : WF Γ (A 𝕏 B) ab → WF Γ Ω (ab ∈ F) := λ _, (by WF_prover)
+  
+  -- retrieve proof of F ⊆ α × β
+  def graph.is_subset {A B α β} (F :@graph A B α β)
+    : ⊨ (↑F ⊆ (α 𝕏 β)) := (F.property).1
+  
+  -- retrieve proof of functionality "F : α → β"
+  def graph.is_functional {A B α β} (F :@graph A B α β)
+    : ⊨ (∀' A ((↑0 ∈ α) ⟹ (∃!' B $ ⟪↑1,↑0⟫ ∈ F))) := (F.property).2
+
+  -- the identity graph
+  def diagonal {A} (α : tset A) : graph α α :=
+    ( graph.mk 
+      ( tset.mk (A 𝕏 A) ⟦ A 𝕏 A | ∃' A (↑1 ≃[A 𝕏 A] ⟪↑0,↑0⟫) ⟧
+        (by apply_rules [WF_rules, WF.closed_add_context];refl)
+      )
+    ) 
+    (by sorry)
+
+end
+
+/-! ### composition -/
+
+section
+
+  variables {A B C D : type}
+  variable {α : tset A}
+  variable {β : tset B}
+  variable {η : tset C}
+  variable {δ : tset D}
+
+  /- The underlying term of the composition of two graphs -/
+  def composition_term (F : graph α β) (G : graph β η) : term :=
+    ⟦ A 𝕏 C | -- all d : A × C such that
+              ∃[A,C] -- ∃ a c,
+              (
+                (↑2 ≃[A 𝕏 C] ⟪↑1,↑0⟫) -- d = ⟨a,c⟩
+                ⋀
+                (∃' B ((⟪↑2,↑0⟫ ∈ F) ⋀ (⟪↑0, ↑1⟫ ∈ G))) -- ∃ b, ⟨a,b⟩ ∈ F ∧ ⟨b,c⟩ ∈ G
+              )
+    ⟧
+  
+  /- The composition construction is a well-formed term -/
+  @[WF_rules]
+  def WF.composition (F : graph α β) (G : graph β η) : closed (𝒫 (A 𝕏 C)) (composition_term F G) :=
+    by WF_prover;refl
+
+  /- The graph which is the composition of two graphs 
+  Note we define F ∘ G as what would usually be G ∘ F (this is just the Lean convention) -/
+  def composition (F : graph α β) (G : graph β η) : graph α η :=
+    (graph.mk (tset.mk (A 𝕏 C) (composition_term F G) (WF.composition F G)))
+    (by sorry)
+
+  /- (F ∘ G) ∘ H ≃ F ∘ (G ∘ H) -/
+  def associativity (F : graph α β) (G : graph β η) (H : graph η δ) :
+      ⊨ (composition (composition F G) H ≃[𝒫 (A 𝕏 D)] composition F (composition G H)) 
+    := sorry
+
+  /- F ∘ Δ_β ≃ F -/
+  def comp_id (F : graph α β) : ⊨ composition F (diagonal β) ≃ F
+    := sorry
+
+  /- Δ_α ∘ F ≃ F -/
+  def id_comp (F : graph α β) : ⊨ composition (diagonal α) F ≃ F
+    := sorry
 
 end
 
@@ -71,20 +155,18 @@ end TT
 
 namespace category_theory
 
-namespace TT
-
-open TT
-
--- instance category : small_category (Σ A : type, tset A) :=
--- { hom := λ ⟨A,α⟩ ⟨B,β⟩, graph α β,
---   id := λ ⟨A,α⟩, id_graph α,
---   -- comp := λ A B E F G, composition F G,
---   -- id_comp' := λ C D F, by cases F; refl,
---   -- comp_id' := λ C D F, by cases F; refl,
---   -- assoc' := by intros; refl
---   }
-
-
-end TT
+/- The category of tsets
+  Objects : Dependent pairs ⟨A,α⟩ : (Σ A : TT.type, TT.tset A)
+            with (A : TT.type) and (α : TT.tset A)
+  Morphisms : Graphs between tsets (with id_graph as identity)
+ -/
+instance TT.category : small_category (Σ A : TT.type, TT.tset A) :=
+{ hom := λ X Y, TT.graph X.2 Y.2,
+  id := λ X, TT.diagonal X.2,
+  comp := λ _ _ _ F G, TT.composition F G,
+  id_comp' := sorry,
+  comp_id' := sorry,
+  assoc' := sorry
+  }
 
 end category_theory
--- src/lemmas.lean
+++ src/lemmas.lean
@@ -1,73 +1,56 @@
-import TL
-import tactic.tidy
+import definitions
+import wellformedness
+import tactic
 
 namespace TT
-
-open proof
-
 
 variables p q r φ ψ : term
 variable {Γ : context}
 
-lemma from_meta_imp (_ : WF Γ Ω φ) (_ : WF Γ Ω ψ) : (proof Γ ⊤ φ → proof Γ ⊤ ψ) → proof Γ φ ψ
-  := sorry
-
-lemma to_meta_imp : proof Γ φ ψ  → proof Γ ⊤ φ → proof Γ ⊤ ψ :=
-  by {intros, apply proof.cut _ φ _, tidy}
-
-lemma from_imp {Γ : context} : proof Γ ⊤ (q ⟹ r) → proof Γ q r :=
+lemma from_imp {Γ : context} : entails Γ ⊤ (q ⟹ r) → entails Γ q r :=
 begin
   intro h₁,
-  apply proof.cut _ (⊤ ⋀ q) _,
-  apply_rules easy_proofs,
-  repeat {
-    apply @WF.imp_left _ q r,
-    exact WF.proof_right _ _ h₁
-  },
-  exact proof.imp_to_and h₁,
+  apply entails.cut _ (⊤ ⋀ q) _,
+  apply_rules [entails.and_intro, entails.vac, entails.axm];
+    { apply @WF.imp_left _ q r,
+      exact WF.proof_right h₁
+    },
+  exact entails.imp_to_and h₁,
 end
 
-lemma to_imp {Γ : context} : proof Γ q r → proof Γ ⊤ (q ⟹ r) :=
+lemma to_imp {Γ : context} : entails Γ q r → entails Γ ⊤ (q ⟹ r) :=
 begin
   intro h₁,
-  apply proof.and_to_imp,
-  apply proof.cut _ q _,
-  apply proof.and_right _ ⊤ _,
-  apply proof.axm,
-  apply WF.and,
-  exact WF.top,
-  apply WF.proof_left q r,
-  tidy
+  apply_rules [entails.and_to_imp, entails.cut _ q _, entails.and_right _ ⊤ _, entails.axm],
+  WF_prover,
+  apply WF.proof_left h₁,
 end
-lemma proof.or_inl (wfq : WF Γ Ω q) (prfp :proof Γ ⊤ p) : proof Γ ⊤ (p ⋁ q) :=
-  by {apply proof.cut _ p _, assumption, apply proof.or_left _ q, apply proof.axm, apply_rules [WF.or, WF.proof_right]}
-lemma proof.or_inr (wfq : WF Γ Ω p) (prfp :proof Γ ⊤ q) : proof Γ ⊤ (p ⋁ q) :=
-  by {apply proof.cut _ q _, assumption, apply proof.or_right _ q, apply proof.axm, apply_rules [WF.or, WF.proof_right]}
+lemma entails.or_inl (wfq : WF Γ Ω q) (prfp :entails Γ ⊤ p) : entails Γ ⊤ (p ⋁ q) :=
+  by {apply entails.cut _ p _, assumption, apply entails.or_left _ q, apply entails.axm, apply_rules [WF.or, WF.proof_right]}
+lemma entails.or_inr (wfq : WF Γ Ω p) (prfp :entails Γ ⊤ q) : entails Γ ⊤ (p ⋁ q) :=
+  by {apply entails.cut _ q _, assumption, apply entails.or_right _ q, apply entails.axm, apply_rules [WF.or, WF.proof_right]}
 
-lemma proof_of_and_left (_ : WF Γ Ω p) (_ : WF Γ Ω q) : proof Γ (p ⋀ q) p :=
-  by {intros, apply proof.and_left _ p q, apply proof.axm, apply WF.and, tidy}
-lemma proof_of_and_right (_ : WF Γ Ω p) (_ : WF Γ Ω q) : proof Γ (p ⋀ q) q :=
-  by {apply proof.and_right _ p q, apply proof.axm, apply WF.and, tidy}
+lemma proof_of_and_left (_ : WF Γ Ω p) (_ : WF Γ Ω q) : entails Γ (p ⋀ q) p :=
+  by {intros, apply entails.and_left _ p q, apply entails.axm, apply WF.and, tidy}
+lemma proof_of_and_right (_ : WF Γ Ω p) (_ : WF Γ Ω q) : entails Γ (p ⋀ q) q :=
+  by {apply entails.and_right _ p q, apply entails.axm, apply WF.and, tidy}
 
-example (_ : WF Γ Ω p) (_ : WF Γ Ω q) : proof Γ (p ⋀ q) (q ⋀ p) :=
+example (_ : WF Γ Ω p) (_ : WF Γ Ω q) : entails Γ (p ⋀ q) (q ⋀ p) :=
 begin
-  apply proof.and_intro,
+  apply entails.and_intro,
   apply proof_of_and_right,
   tidy,
   apply proof_of_and_left,
   tidy
 end
 
-namespace proof
-open term
+lemma eq_sound {A : type} {a₁ a₂ : term} (eq : ⊨ (a₁ ≃[A] a₂)) (φ : term) : entails Γ ⊤ ⁅φ // a₁⁆ → entails Γ ⊤ ⁅φ // a₂⁆ :=
+by sorry
 
-lemma eq_sound {A : type} {a₁ a₂ : term} (eq : ⊨ (a₁ ≃[A] a₂)) (φ ψ : term) : proof Γ ⊤ ⁅φ // a₁⁆ → proof Γ ⊤ ⁅φ // a₂⁆ :=
-  by sorry
-
-lemma reverse_extensionality (A : type) : ⊨ (∀' (𝒫 A) $ ∀' (𝒫 A) $ (𝟙 ≃[𝒫 A] 𝟘) ⟹ (∀' A ((𝟘 ∈ 𝟙) ⇔ (𝟘 ∈ 𝟚)))) :=
+lemma reverse_extensionality (A : type) : ⊨ (∀' (𝒫 A) $ ∀' (𝒫 A) $ (↑1 ≃[𝒫 A] ↑0) ⟹ (∀' A ((↑0 ∈ ↑2) ⇔ (↑0 ∈ ↑1)))) :=
 begin
-  apply proof.all_intro 𝒫 A,
-  apply proof.all_intro 𝒫 A,
+  apply entails.all_intro 𝒫 A,
+  apply entails.all_intro 𝒫 A,
   apply to_imp,
   apply from_meta_imp,
   any_goals {apply_rules WF_rules; refl},
@@ -75,9 +58,40 @@ begin
   sorry
 end
 
+def is_star {Γ : context} {a : term} : WF Γ 𝟙 a → entails Γ ⊤ (a ≃[𝟙] ⁎) :=
+begin
+  intro wfa,
+  apply entails.sub 𝟙 a ⊤ (↑0 ≃[𝟙] ⁎),
+  assumption,
+  have : (⊤ : term) = ^ ⊤, by rw WF.lift_closed; exact WF.top,
+  rw this,
+  apply entails.all_elim,
+  rw ←list.nil_append Γ,
+  apply entails.weakening,
+  exact entails.star_unique
+end
 
+local notation `[]` := list.nil
 
-end proof
+lemma phi_ent_phi_and_top (h : WF [] Ω φ): φ ⊨ (φ ⋀ ⊤) :=
+begin
+  apply entails.and_intro,
+  apply entails.axm,
+  exact h,
+  apply entails.vac,
+  exact h
+end
+
+section meta_conversion
+
+lemma ent_to_meta {p} {wfP : WF [] Ω p} : entails Γ φ ψ  → entails Γ p φ → entails Γ p ψ :=
+  λ _ _, (by {apply entails.cut _ φ _, tidy})
+
+lemma meta_to_ent (wfφ : WF Γ Ω φ) : (∀ p, entails Γ p φ → entails Γ p ψ) → entails Γ φ ψ :=
+  λ h, h φ (entails.axm wfφ)
+
+end meta_conversion
+
 
 
 end TT
